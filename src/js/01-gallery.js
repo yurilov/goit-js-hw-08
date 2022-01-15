517cbd6925489e9403de81a6856d921769aca15d
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-01-15 13:20:15 +0200

    added vimeo player

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,13 +7,13 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const galleryRef = document.querySelector('.gallery');
 
 function createGallery(items) {
-  for (const item of items) {
+  items.map(item => {
     const markup = `<a class="gallery__item" href="${item.original}">
     <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
   </a>`;
 
     galleryRef.insertAdjacentHTML('beforeend', markup);
-  }
+  });
 }
 
 createGallery(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', onPlay);
+
+player.on('timeupdete', onTimeUpdate);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+function onPlay(data) {
+  console.log('onPlay - data', data);
+}
+
+function onTimeUpdate(data) {
+  localStorage.setItem('videoplayer-current-time', data);
+}
