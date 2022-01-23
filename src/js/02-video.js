73ea6072f2d96cece8e212fde3cd898948b21cec
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-01-23 11:35:41 +0200

    completed second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
+
+const LOCAL_TIME_SET = 'videoplayer-current-time';
+const localTime = localStorage.getItem(LOCAL_TIME_SET);
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('play', onPlay);
-
-player.on('timeupdete', onTimeUpdate);
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
-function onPlay(data) {
-  console.log('onPlay - data', data);
+if (localTime) {
+  player.setCurrentTime(localTime);
 }
 
-function onTimeUpdate(data) {
-  localStorage.setItem('videoplayer-current-time', data);
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate({ seconds }) {
+  localStorage.setItem(LOCAL_TIME_SET, seconds);
 }
+
+// player.on('play', onPlay);
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+// function onPlay(data) {
+//   console.log('onPlay - data', data);
+// }
